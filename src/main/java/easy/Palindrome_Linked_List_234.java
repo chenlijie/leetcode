6afc07cb29337e6cfe2faa897262a92a88c8be89
chenlijie <chenlijie139@gmail.com>
2017-05-07 21:43:07 -0500

    delete build file

--- src/main/java/easy/Palindrome_Linked_List_234.java
+++ src/main/java/easy/Palindrome_Linked_List_234.java
@@ -13,6 +13,58 @@ public class Palindrome_Linked_List_234 {
 
     public boolean isPalindrome(ListNode head) {
 
-        return false;
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        fast = revertList(slow.next);
+        slow = head;
+
+        while (fast != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return true;
+    }
+
+    private ListNode revertList(ListNode head) {
+        ListNode prev = null;
+        ListNode next;
+
+        while (head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Palindrome_Linked_List_234.ListNode node1 = new Palindrome_Linked_List_234.ListNode(1);
+        Palindrome_Linked_List_234.ListNode node2 = new Palindrome_Linked_List_234.ListNode(2);
+        Palindrome_Linked_List_234.ListNode node3 = new Palindrome_Linked_List_234.ListNode(3);
+        Palindrome_Linked_List_234.ListNode node4 = new Palindrome_Linked_List_234.ListNode(2);
+        Palindrome_Linked_List_234.ListNode node5 = new Palindrome_Linked_List_234.ListNode(1);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        System.out.println(new Palindrome_Linked_List_234().isPalindrome(node4));
     }
 }
